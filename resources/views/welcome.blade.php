736060646a5f298add646661121cc402c96d60f0
RayMuchammad <m.raynaldi.st@gmail.com>
2021-01-12 08:39:03 +0700

    update: image welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
 <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
     <div class="md:flex">
         <div class="md:flex-shrink-0">
-            <img class="h-48 w-full object-cover md:w-48" src="https://picsum.photos/seed/picsum/200/300" alt="Man looking at item at a store">
+            <img class="h-48 w-full object-cover md:w-48" src="https://picsum.photos/200/300?grayscale" alt="Man looking at item at a store">
         </div>
         <div class="p-8">
             <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
@@ -61,7 +61,7 @@
 <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
     <div class="md:flex">
         <div class="md:flex-shrink-0">
-            <img class="h-48 w-full object-cover md:w-48" src="https://picsum.photos/seed/picsum/200/300" alt="Man looking at item at a store">
+            <img class="h-48 w-full object-cover md:w-48" src="https://picsum.photos/200/300/?blur" alt="Man looking at item at a store">
         </div>
         <div class="p-8">
             <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
